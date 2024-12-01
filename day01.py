d5b2c6802e42ad04c484e7e05c503c517b2eb48b
slow-connect <louisdeberlin+github@gmail.com>
2024-12-01 12:44:36 +0100

    day 1

---
+++ day01.py
@@ -0,0 +1,33 @@
+import aoc
+from numpy import abs
+from collections import Counter
+
+data = aoc.get_lst(1)[:-1]
+def p1(data):
+    l1, l2 = [], []
+
+    for i in range(len(data)):
+        tmp = data[i].split()
+        l1.append(int(tmp[0]))
+        l2.append(int(tmp[1]))
+    l1.sort()
+    l2.sort()
+    res = 0
+    for i in range(len(data)):
+        res += abs(l1[i] - l2[i])
+    print(res)
+
+def p2(data):
+    l1, l2 = [], []
+    for i in range(len(data)):
+        tmp = data[i].split()
+        l1.append(int(tmp[0]))
+        l2.append(int(tmp[1]))
+    l2 = Counter(l2)
+    res = 0
+    for val in l1:
+        if val in l2:
+            res += val * l2[val]
+    print(res)
+
+p2(data)
